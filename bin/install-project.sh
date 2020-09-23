f388be87201f1226047c62ec943cc115c49f54b6
@geo_inca <geo.inca@hotmail.com>
2020-09-24 00:23:34 +0200

    [Ticket: 20200924]
    	modified:   bin/install-project.sh
    	modified:   docker-compose.yml
    	modified:   docker/conf/nginx.conf
    	modified:   docker/conf/php.ini

--- bin/install-project.sh
+++ bin/install-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Clean up existing docker data -For clean installation
+rm -rf laravelminio
 rm -rf docker/laravel
 rm -rf docker/db/dbdata
 
@@ -9,17 +10,18 @@ docker-compose down
 # OPTION 1
 # -------------------- #
 # Download laravel LTS framework (https://github.com/laravel/laravel/releases)
-LARAVEL_VERSION_NO="5.5.28"
-curl -L https://github.com/laravel/laravel/archive/v${LARAVEL_VERSION_NO}.tar.gz | tar xz
-mv laravel-${LARAVEL_VERSION_NO} docker/laravel
-cp docker/laravel/.env.example docker/laravel/.env
+#LARAVEL_VERSION_NO="5.5.28"
+#curl -L https://github.com/laravel/laravel/archive/v${LARAVEL_VERSION_NO}.tar.gz | tar xz
+#mv laravel-${LARAVEL_VERSION_NO} docker/laravel
+#cp docker/laravel/.env.example docker/laravel/.env
 # -------------------- #
 
 # OPTION 2
 # -------------------- #
 # OR clone your existing project
-#git clone http://gitlab.apnic.net/laravel/laravel-laravel.git laravel
-#mv laravel docker/laravel
+git clone https://github.com/geoinca/laravelminio.git 
+
+mv laravelminio/laravelminio docker/laravel
 
 # Checkout develop branch for local development purpose
 #cd laravel
